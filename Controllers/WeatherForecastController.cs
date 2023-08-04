ef153ca821fc346ab26c5fabab17360e6c32d7cb
Marlon Couto <devpatola@proton.me>
2023-08-03 22:48:00 -0300

    Remove comentários

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,24 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 
-// Declaração de namespace.
 namespace ContosoPizza.Controllers;
 
-// Indica que este é controlador de API.
-// Permite a vinculação automática dos parâmetros de solicitação HTTP para os parâmetros do método.
-// Habilita comportamentos adicionais que facilitam a criação de APIs Web.
 [ApiController]
-// Define a rota do controlador.
-// [controller] é uma variável de espaço reservada que será substituída pelo nome do controlador.
-// O token "controller" é substituído pelo nome do controlador, sem diferenciação de maiúsculas e minúsculas.
-// A palavra "Controller" não faz parte do nome da rota gerada por esse token.
-// Ex: aqui é gerada a rota /weatherforecast.
-// A rota poderia conter caracteres estáticos, como api/[controller].
-// Neste caso, a rota seria /api/weatherforecast.
 [Route("[controller]")]
-// Herda da classe ControllerBase (classe base para controladores em ASP.NET Core).
 public class WeatherForecastController : ControllerBase
 {
-    // Matriz de strings contendo diferentes descrições climáticas.
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -26,35 +13,20 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
-    // Construtor que recebe uma instância como parâmetro, usada para realizar o registro de informações.
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
     }
 
-    // Este atributo encaminha solicitações Get para o método "public IEnumerable<WeatherForecast> Get() {}".
     [HttpGet(Name = "GetWeatherForecast")]
-    // O método Get() é um endpoint HTTP GET que retorna uma coleção de WeatherForecast.
-    // Neste caso, é uma lista de previsões do tempo geradas aleatoriamente para os próximos 5 dias.
     public IEnumerable<WeatherForecast> Get()
     {
-        // Cria um range de números inteiros de 1 a 5 e, em seguida, realiza uma seleção para cada número.
-        // Depois cria uma nova instância da classe WeatherForecast e inicializa sua propriedade Date.
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
-            // A data é definida como a data e hora atual (DateTime.Now) mais o valor do índice.
             Date = DateTime.Now.AddDays(index),
-            // As previsões são geradas usando a classe Random.
-            // A classe Random é uma classe estática que fornece métodos para gerar números aleatórios.
-            // O método Next() da classe Random retorna um número inteiro no intervalo especificado (-20 a 54).
-            // O valor retornado é a temperatura em graus Celsius.
             TemperatureC = Random.Shared.Next(-20, 55),
-            // Escolhe aleatoriamente um resumo do clima da matriz Summaries.
-            // O método Next() da classe Random é usado para gerar um índice aleatório dentro dos limites do tamanho da matriz.
-            // O resumo é selecionado aleatoriamente com base no índice gerado.
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
-        // Converte o resultado da seleção em uma matriz de objetos WeatherForecast.
         .ToArray();
     }
 }
--- Services/PizzaServices.cs
+++ Services/PizzaServices.cs
@@ -1,19 +1,12 @@
-// Importa o namespace ContosoPizza.Models, que contém a definição da classe Pizza.
 using ContosoPizza.Models;
 
-// Define o namespace ContosoPizza.Services para o arquivo atual.
 namespace ContosoPizza.Services
 {
-    // Cria uma classe estática chamada PizzaService.
     public static class PizzaService
     {
-        // Cria uma lista estática de objetos do tipo Pizza para armazenar as pizzas.
         static List<Pizza> Pizzas { get; }
-        // Cria uma variável estática nextId e atribui o valor 3.
         static int nextId = 3;
 
-        // Construtor estático da classe PizzaService.
-        // Inicializa a lista Pizzas com duas instâncias de Pizza usando a sintaxe de inicialização de coleções.
         static PizzaService()
         {
             Pizzas = new List<Pizza>
@@ -23,38 +16,27 @@ namespace ContosoPizza.Services
             };
         }
 
-        // Método estático que retorna todas as pizzas contidas na lista.
         public static List<Pizza> GetAll() => Pizzas;
 
-        // Método estático que retorna uma pizza com base no ID fornecido ou null se não encontrada.
         public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
 
-        // Método estático que adiciona uma pizza à lista, atribuindo um novo ID a ela.
         public static void Add(Pizza pizza)
         {
             pizza.Id = nextId++;
             Pizzas.Add(pizza);
         }
 
-        // Método estático que remove uma pizza com base no ID fornecido, se encontrada.
         public static void Delete(int id)
         {
-            // Verifica se a pizza com o ID fornecido existe na lista.
-            // Se não existir, retorna sem fazer nada.
             var pizza = Get(id);
             if (pizza is null) return;
-            // Remove a pizza da lista.
             Pizzas.Remove(pizza);
         }
 
-        // Método estático que atualiza as informações de uma pizza existente na lista.
         public static void Update(Pizza pizza)
         {
-            // Procura o índice da pizza com o ID fornecido na lista.
             var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
-            // Se a pizza não for encontrada (índice igual a -1), retorna sem fazer nada.
             if (index == -1) return;
-            // Atualiza a pizza na lista com base no índice encontrado.
             Pizzas[index] = pizza;
         }
     }
